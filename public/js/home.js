b6f4ff270cf0a145243454ef7d99d53a25892464
Annab111 <78909410+Annab111@users.noreply.github.com>
2022-05-07 19:19:30 +0100

    Add files via upload

---
+++ public/js/home.js
@@ -0,0 +1,3 @@
+const mailContainer = document.querySelector('.mail-container');
+const shownMailContainer = 'container mail-container shown-container';
+const hiddenMailContainer = 'container mail-container hidden-container';
